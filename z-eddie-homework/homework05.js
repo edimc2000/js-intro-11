37b08fdd0d816b55230a2dd044194346e8ba221a
Eddie C. <edimc2000@gmail.com>
2025-03-11 11:56:39 -0500

    t2

--- z-eddie-homework/homework05.js
+++ z-eddie-homework/homework05.js
@@ -4,7 +4,7 @@ Requirement:
  and returns how many elements are positive​ when invoked. 
 Examples:
  countPos([-45, 0, 0, 34, 5, 67])       -> 3
- countPos([-23, -4, 0, 2, 5, 90, 123]) 	-> 4
+ countPos([-23, -4, 0, 2, 5, 90, 123])  -> 4
  countPos([0, -1, -2, -3])              -> 0
 */
 
