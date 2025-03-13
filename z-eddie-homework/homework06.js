3824b0c708714511e2f8d888e96d6de96a3980f5
Eddie C. <edimc2000@gmail.com>
2025-03-13 09:55:32 -0500

    initial upload

---
+++ z-eddie-homework/homework06.js
@@ -0,0 +1,37 @@
+/* Task 1 
+Requirement:
+ Write a function named noSpace() which takes a string as argument and returns a new string with all the spaces removed.
+Examples:
+ noSpace("") 			        -> ""
+ noSpace("Javascript") 		    -> "Javascript"
+ noSpace("    Hello   ") 		-> "Hello"
+ noSpace(" Hello World   ") 	-> "HelloWorld”
+ noSpace("Tech Global") 		-> "TechGlobal"
+*/
+console.log('--- Task 1 ---'); 
+const noSpace = string => string.trim(); 
+
+console.log('noSpace("")                ->', noSpace("") );
+console.log('noSpace("Javascript")      ->', noSpace("Javascript") );
+console.log('noSpace("  Hello  ")       ->', noSpace("  Hello  ") );
+console.log('noSpace(" Hello World  ")  ->', noSpace(" Hello World  ") );
+console.log('noSpace("Tech Global")     ->', noSpace("Tech Global") );
+
+/* Task 1 
+Requirement:
+ Write a function named noSpace() which takes a string as argument and returns a new string with all the spaces removed.
+Examples:
+ noSpace("") 			        -> ""
+ noSpace("Javascript") 		    -> "Javascript"
+ noSpace("    Hello   ") 		-> "Hello"
+ noSpace(" Hello World   ") 	-> "HelloWorld”
+ noSpace("Tech Global") 		-> "TechGlobal"
+*/
+console.log('--- Task 1 ---'); 
+const noSpace = string => string.trim(); 
+
+console.log('noSpace("")                ->', noSpace("") );
+console.log('noSpace("Javascript")      ->', noSpace("Javascript") );
+console.log('noSpace("  Hello  ")       ->', noSpace("  Hello  ") );
+console.log('noSpace(" Hello World  ")  ->', noSpace(" Hello World  ") );
+console.log('noSpace("Tech Global")     ->', noSpace("Tech Global") );
