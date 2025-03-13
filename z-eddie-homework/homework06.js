0dae36e708524004ce3c5d376e39d5dd327bfca4
Eddie C. <edimc2000@gmail.com>
2025-03-13 10:41:56 -0500

    added task 5

--- z-eddie-homework/homework06.js
+++ z-eddie-homework/homework06.js
@@ -79,19 +79,39 @@ Examples:
  checkAge(1920) -> "AGE IS ALLOWED"
  checkAge(1800) -> "AGE IS NOT VALID"
 */
-console.log('--- Task 4 ---'); 
+// console.log('--- Task 4 ---'); 
+// const checkAge = yearOfBirth => {
+//     const age = Number(new Date( Date.now()).toDateString().slice(-4)) - yearOfBirth;
+//     return age < 0 ? 'AGE IS NOT VALID '
+//      : age < 16 ? 'AGE IS NOT ALLOWED'
+//      : age > 16 && age < 120 ? 'AGE IS ALLOWED'
+//      : age > 120 ? 'AGE IS NOT VALID'
+//      : null; 
+// }
 
-const checkAge = yearOfBirth => {
-    const age = Number(new Date( Date.now()).toDateString().slice(-4)) - yearOfBirth;
-    return age < 0 ? 'AGE IS NOT VALID '
-     : age < 16 ? 'AGE IS NOT ALLOWED'
-     : age > 16 && age < 120 ? 'AGE IS ALLOWED'
-     : age > 120 ? 'AGE IS NOT VALID'
-     : null; 
-}
+// console.log('checkAge(2015) -> ', checkAge(2015)); 
+// console.log('checkAge(2007) -> ', checkAge(2007)); 
+// console.log('checkAge(2050) -> ', checkAge(2050)); 
+// console.log('checkAge(1920) -> ', checkAge(1920)); 
+// console.log('checkAge(1800) -> ', checkAge(1800)); 
 
-console.log('checkAge(2015) -> ', checkAge(2015)); 
-console.log('checkAge(2007) -> ', checkAge(2007)); 
-console.log('checkAge(2050) -> ', checkAge(2050)); 
-console.log('checkAge(1920) -> ', checkAge(1920)); 
-console.log('checkAge(1800) -> ', checkAge(1800)); 
+
+/* Task 5
+Requirement:
+ Write a function named averageOfEdges() which takes three number arguments 
+ and will return average of min and max numbers​.
+Examples:
+ averageOfEdges(0, 0, 0)     -> 0
+ averageOfEdges(0, 0, 6)     -> 3
+ averageOfEdges(-2, -2, 10)  -> 4
+ averageOfEdges(-3, 15, -3)  -> 6
+ averageOfEdges(10, 13, 20)  -> 15
+*/
+console.log('--- Task 5 ---'); 
+const averageOfEdges = (num1, num2, num3) => (Math.min(num1, num2, num3) + Math.max(num1, num2, num3)) / 2;
+
+console.log('averageOfEdges(0, 0, 0)    -> ', averageOfEdges(0, 0, 0)); 
+console.log('averageOfEdges(0, 0, 6)    -> ', averageOfEdges(0, 0, 6)); 
+console.log('averageOfEdges(-2, -2, 10) -> ', averageOfEdges(-2, -2, 10)); 
+console.log('averageOfEdges(-3, 15, -3) -> ', averageOfEdges(-3, 15, -3)); 
+console.log('averageOfEdges(10, 13, 20) -> ', averageOfEdges(10, 13, 20)); 
