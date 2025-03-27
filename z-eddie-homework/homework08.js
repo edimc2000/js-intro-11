9ee720300fcf498e050234f5e5bc43ccc9426958
Eddie C. <edimc2000@gmail.com>
2025-03-27 17:27:58 -0500

    added Homework8

---
+++ z-eddie-homework/homework08.js
@@ -0,0 +1,66 @@
+/* Task 1 
+Requirement:
+ Write a function named hasLowerCase() which takes a string argument 
+ and returns true if there is a lowercase letter and false if it there isn’t. 
+Examples:
+ hasLowerCase("")            -> false
+ hasLowerCase("JAVASCRIPT")  -> false
+ hasLowerCase("hello")       -> true
+ hasLowerCase("125$")        -> false
+ hasLowerCase("   a   ")     -> true
+*/
+console.log ('--- Task 1 ---')
+const hasLowerCase = string => /[a-z]/.test(string);
+
+let arrOfGivens = [ '', 'JAVASCRIPT', 'hello', '125$', '   a   ' ]; 
+for (let element of arrOfGivens){
+    console.log(`hasLowerCase('${element}')`.padEnd(30,' '), '->', hasLowerCase(element)); 
+}
+
+
+/* Task 2
+Requirement:
+ Write a function named noZero() which takes an array of numbers as argument 
+ and returns the array back with all zeros removed.
+Examples:
+ noZero([1])           -> [1]
+ noZero([1, 1, 10]​)    -> [1, 1, 10]​
+ noZero([0, 1, 10])    -> [1, 10]
+ noZero([0, 0, 0])     -> []
+ noZero([10, 100, 0])  -> [10, 100]
+*/
+console.log ('\n--- Task 2 ---')
+const noZero = arr => arr.filter(e => e > 0); 
+
+arrOfGivens = [ [1], [1, 1, 10], [0, 1, 10], [0, 0, 0], [10, 100, 0] ]; 
+for (let element of arrOfGivens){
+     console.log(`noZero([ ${element.join(', ')} ])`.padEnd(30,' '), '->', noZero(element)); 
+ }
+ 
+
+
+
+/* Task 3
+Requirement:
+ Write a function named numberAndSquare() which takes an array of numbers as argument
+ and returns a multidimensional array with all numbers squared. 
+Examples:
+ numberAndSquare([1, 2, 3])     -> [[1, 1], [2, 4], [3, 9]]
+ numberAndSquare([0, 3, -6])    -> [[0, 0], [3, 9], [-6, 36]]
+ numberAndSquare([1, 4])        -> [[1,1], [4, 16]]
+ numberAndSquare([0, 0, 0])     -> [[0, 0], [0, 0], [0, 0]]
+ numberAndSquare([0, 1, -10])   -> [[0, 0], [1, 1], [-10, 100]]
+*/
+console.log ('\n--- Task 3 ---')
+
+arrOfGivens = [ [1, 2, 3], [0, 3, -6], [1, 4], [0, 0, 0], [0, 1, -10] ]; 
+for (let element of arrOfGivens){
+     console.log(`noZero([ ${element.join(', ')} ])`.padEnd(30,' '), '->', noZero(element)); 
+ }
+ 
+
+ 
+/* Task 4
+
+*/
+console.log ('\n--- Task 4 ---')
