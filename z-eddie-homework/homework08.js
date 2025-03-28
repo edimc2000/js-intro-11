a1bc8ce654ef7bf3007b956b8652c71a7742d045
Eddie C. <edimc2000@gmail.com>
2025-03-28 11:00:36 -0500

    cleaned up

--- z-eddie-homework/homework08.js
+++ z-eddie-homework/homework08.js
@@ -9,12 +9,12 @@ Examples:
  hasLowerCase("125$")        -> false
  hasLowerCase("   a   ")     -> true
 */
-console.log ('--- Task 1 ---');
-const hasLowerCase = string => /[a-z]/.test(string);
+console.log("--- Task 1 ---");
+const hasLowerCase = (string) => /[a-z]/.test(string);
 
-let arrOfGivens = [ '', 'JAVASCRIPT', 'hello', '125$', '   a   ' ]; 
-for (let element of arrOfGivens){
-    console.log(`hasLowerCase('${element}')`.padEnd(30,' '), '->', hasLowerCase(element)); 
+let arrOfGivens = ['', 'JAVASCRIPT', 'hello', '125$', '   a   '];
+for (let element of arrOfGivens) {
+    console.log(`hasLowerCase('${element}')`.padEnd(30, ' '), '->', hasLowerCase(element));
 }
 
 /* Task 2
@@ -28,14 +28,14 @@ Examples:
  noZero([0, 0, 0])     -> []
  noZero([10, 100, 0])  -> [10, 100]
 */
-console.log ('\n--- Task 2 ---');
-const noZero = arr => arr.filter(e => e > 0); 
+console.log('\n--- Task 2 ---');
+const noZero = arr => arr.filter(e => e > 0);
 
-arrOfGivens = [ [1], [1, 1, 10], [0, 1, 10], [0, 0, 0], [10, 100, 0] ]; 
-for (let element of arrOfGivens){
-     console.log(`noZero([ ${element.join(', ')} ])`.padEnd(30,' '), '->', noZero(element)); 
- }
- 
+arrOfGivens = [[1], [1, 1, 10], [0, 1, 10], [0, 0, 0], [10, 100, 0]];
+for (let element of arrOfGivens) {
+    console.log(`noZero([ ${element.join(', ')} ])`.padEnd(30, ' '), '->', noZero(element));
+}
+
 /* Task 3
 Requirement:
  Write a function named numberAndSquare() which takes an array of numbers as argument
@@ -47,14 +47,14 @@ Examples:
  numberAndSquare([0, 0, 0])     -> [[0, 0], [0, 0], [0, 0]]
  numberAndSquare([0, 1, -10])   -> [[0, 0], [1, 1], [-10, 100]]
 */
-console.log ('\n--- Task 3 ---');
-const numberAndSquare = arr => arr.map (e=> ([e, e ** 2]));
+console.log('\n--- Task 3 ---');
+const numberAndSquare = arr => arr.map(e => ([e, e ** 2]));
 
-arrOfGivens = [ [1, 2, 3], [0, 3, -6], [1, 4], [0, 0, 0], [0, 1, -10] ]; 
-for (let element of arrOfGivens){
-    console.log(`numberAndSquare([ ${element.join(', ')} ])`.padEnd(30,' '), '->', numberAndSquare(element));  
+arrOfGivens = [[1, 2, 3], [0, 3, -6], [1, 4], [0, 0, 0], [0, 1, -10]];
+for (let element of arrOfGivens) {
+    console.log(`numberAndSquare([ ${element.join(', ')} ])`.padEnd(30, ' '), '->', numberAndSquare(element));
 }
- 
+
 /* Task 4
 Requirement:
  Write a function named containsValue() which takes a string array and a string as arguments and 
@@ -67,14 +67,14 @@ Examples:
  containsValue(["abc", "def", "123"], "Abc")                         -> false
  containsValue(["abc", "def", "123", "Javascript", "Hello"], "123")  -> true
 */
-console.log ('\n--- Task 4 ---');
+console.log('\n--- Task 4 ---');
 const containsValue = (arr, string) => arr.includes(string);
 
-arrOfGivens = [ [["abc", "foo", "javascript"], "hello"], [["abc", "def", "123"], "Abc"], [["abc", "def", "123", "Javascript", "Hello"], "123"] ]; 
-for (let element of arrOfGivens){
-    console.log(`containsValue([ '${element[0].join('\', \'')}' ], '${element[1]}')`.padEnd(70,' '), '->', containsValue(element[0], element[1])); 
+arrOfGivens = [[["abc", "foo", "javascript"], "hello"], [["abc", "def", "123"], "Abc"], [["abc", "def", "123", "Javascript", "Hello"], "123"]];
+for (let element of arrOfGivens) {
+    console.log(`containsValue([ '${element[0].join('\', \'')}' ], '${element[1]}')`.padEnd(70, ' '), '->', containsValue(element[0], element[1]));
 }
- 
+
 /* Task 5
 Requirement:
  Write a function named reverseSentence() which takes a string as argument and returns 
@@ -84,14 +84,14 @@ Examples:
  reverseSentence("Javascript is fun")   -> "Fun is javascript"
  reverseSentence("This is a sentence")  -> "Sentence a is this"
 */
-console.log ('\n--- Task 5 ---');
+console.log('\n--- Task 5 ---');
 const capFirstLetter = string => string.replace(/[^.]/, string[0].toUpperCase());
-const reverseSentence = string => string.trim().split(' ').length < 2 ? 'There is not enough words!' 
- : capFirstLetter(string.trim().toLowerCase().split(' ').reverse().join(' '));
+const reverseSentence = string => string.trim().split(' ').length < 2 ? 'There is not enough words!'
+    : capFirstLetter(string.trim().toLowerCase().split(' ').reverse().join(' '));
 
-arrOfGivens = [ 'Hello', 'Javascript is fun', 'This is a sentence' ]; 
-for (let element of arrOfGivens){
-    console.log(`reverseSentence('${element}')`.padEnd(40,' '), '->', reverseSentence(element)); 
+arrOfGivens = ['Hello', 'Javascript is fun', 'This is a sentence'];
+for (let element of arrOfGivens) {
+    console.log(`reverseSentence('${element}')`.padEnd(40, ' '), '->', reverseSentence(element));
 }
 
 /* Task 6
@@ -103,12 +103,12 @@ Examples:
  removeStringSpecialsDigits("Cypress")                  -> "Cypress"
  removeStringSpecialsDigits("Automation123#$%")         -> "Automation"
 */
-console.log ('\n--- Task 6 ---');
+console.log('\n--- Task 6 ---');
 const removeStringSpecialsDigit = string => string.trim().split('').filter(e => /[\sa-z]/i.test(e)).join('');
 
-arrOfGivens = [ '123Javascript #%$is fun', 'Cypress', 'Automation123#$%' ]; 
-for (let element of arrOfGivens){
-    console.log(`removeStringSpecialsDigit('${(element)}')`.padEnd(55,' '), '->', removeStringSpecialsDigit(element)); 
+arrOfGivens = ['123Javascript #%$is fun', 'Cypress', 'Automation123#$%'];
+for (let element of arrOfGivens) {
+    console.log(`removeStringSpecialsDigit('${(element)}')`.padEnd(55, ' '), '->', removeStringSpecialsDigit(element));
 }
 
 /* Task 7
@@ -121,12 +121,12 @@ Examples:
  removeArraySpecialsDigits(["Automation", "123#$%tool"])        -> ["Automation", "tool"]
 
 */
-console.log ('\n--- Task 7 ---');
+console.log('\n--- Task 7 ---');
 const removeArraySpecialsDigits = arr => arr.map(e => e.split('').filter(e => /[\sa-z]/i.test(e)).join(''));
 
-arrOfGivens = [ ["123Javascript", "#%$is", "fun"], ["Cypress", "123$%", "###"], ["Automation", "123#$%tool"] ]; 
-for (let element of arrOfGivens){
-    console.log(`removeArraySpecialsDigits([ ${element.join(', ')} ])`.padEnd(60,' '), '->', removeArraySpecialsDigits(element));  
+arrOfGivens = [["123Javascript", "#%$is", "fun"], ["Cypress", "123$%", "###"], ["Automation", "123#$%tool"]];
+for (let element of arrOfGivens) {
+    console.log(`removeArraySpecialsDigits([ ${element.join(', ')} ])`.padEnd(60, ' '), '->', removeArraySpecialsDigits(element));
 }
 
 /* Task 8
@@ -138,12 +138,12 @@ Examples:
  getCommons( ["Javascript", "is", "fun"], ["Javascript", "C#", "Python"] )  -> ["Javascript"]
  getCommons( ["Javascript", "C#", "C#"], ["Python", "C#", "C++"] )          -> ["C#"]
 */
-console.log ('\n--- Task 8 ---');
+console.log('\n--- Task 8 ---');
 const getCommons = (arr1, arr2) => [...new Set(arr1.filter(e => arr2.includes(e)))];
 
-arrOfGivens = [ [["Javascript", "is", "fun"], ["abc", "xyz", "123"]], [["Javascript", "is", "fun"], ["Javascript", "C#", "Python"]], [["Javascript", "C#", "C#"], ["Python", "C#", "C++"]] ]; 
-for (let element of arrOfGivens){
-    console.log(`getCommons([ '${element[0].join('\', \'')}' ], [ '${element[1].join('\', \'')}' ])`.padEnd(75,' '), '->', getCommons(element[0], element[1])); 
+arrOfGivens = [[["Javascript", "is", "fun"], ["abc", "xyz", "123"]], [["Javascript", "is", "fun"], ["Javascript", "C#", "Python"]], [["Javascript", "C#", "C#"], ["Python", "C#", "C++"]]];
+for (let element of arrOfGivens) {
+    console.log(`getCommons([ '${element[0].join('\', \'')}' ], [ '${element[1].join('\', \'')}' ])`.padEnd(75, ' '), '->', getCommons(element[0], element[1]));
 }
 
 /* Task 9
@@ -159,13 +159,16 @@ Examples:
  noXInVariables(["xyXyxy", "Xx", "ABC"])    -> ["yyy", "ABC"]
 
 */
-console.log ('\n--- Task 9 ---');
+console.log('\n--- Task 9 ---');
 const noXInVariables = arr => {
     const removeX = string => string.split('').filter(e => !'x'.includes(e.toLowerCase())).join('');
-    return arr.map(e => typeof e ==='number' ? e  : removeX(e)).filter(e => e);  
+    return arr.map(e => typeof e === 'number' ? e : removeX(e)).filter(e => e);
 }
 
-arrOfGivens = [ ["abc", 123, "#$%"], ["xyz", 123, "#$%"], ["x", 123, "#$%"], ["xyXyxy", "Xx", "ABC"]  ]; 
-for (let element of arrOfGivens){
-    console.log(`noXInVariables(`, element ,')', `}`, '->'.padStart((16 - String(element).length) ,' '), noXInVariables(element)); 
+arrOfGivens = [ ["abc", 123, "#$%"], ["xyz", 123, "#$%"], ["x", 123, "#$%"], ["xyXyxy", "Xx", "ABC"] ];
+const maxInputLength = Math.max(...arrOfGivens.map(arr => JSON.stringify(arr).length));
+for (let element of arrOfGivens) {
+    const inputStr = `noXInVariables(${JSON.stringify(element)})`;
+    const outputStr = JSON.stringify(noXInVariables(element));
+    console.log(`${inputStr.padEnd(maxInputLength + 20)} -> ${outputStr}`);
 }
