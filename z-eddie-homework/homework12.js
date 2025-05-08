3e68dbd86bb09ea2d94677da2c80c8b1dc2e6296
Eddie C. <edimc2000@gmail.com>
2025-05-08 08:00:35 -0500

    Task 1-5

--- z-eddie-homework/homework12.js
+++ z-eddie-homework/homework12.js
@@ -1,4 +1,4 @@
-const reps = 100, pads = 55
+const reps = 100, pads = 65
 /* Task 1 
 Requirement:
  Write a function named makeNegative() that takes a number and returns its negative value.
@@ -29,7 +29,7 @@ Examples:
 isSumEvenOrOdd(1, 1, 1)    -> "odd"
 */
 console.log('\n--- Task 2 ---' + ('-'.repeat(reps)))
-const isSumEvenOrOdd = (...args) => args.reduce((acc, e) => acc + e, 0) % 2 === 0 ? 'even' :'odd'
+const isSumEvenOrOdd = (...args) => args.reduce((acc, e) => acc + e, 0) % 2 === 0 ? 'even' : 'odd'
 
 arrOfGivens = [[0, 1, 4], [0, -1, -5], [0, 0, 0], [7, 1, 9], [1, 1, 1]]
 for (let element of arrOfGivens) {
@@ -47,12 +47,53 @@ Examples:
  decimal2( [20987, 3.53245, 12.345, 32.9] )  -> [ 20987, 3.53, 12.35, 32.90 ]
  decimal2( [ ] )                             -> [  ]
  decimal2( [4.35623, 8.9742] )               -> [ 4.36, 8.97 ]
-
 */
 console.log('\n--- Task 3 ---' + ('-'.repeat(reps)))
-const decimal2 = arr => arr.map( e => Number(e.toFixed(2)))
+const decimal2 = arr => arr.map(e => Number(e.toFixed(2)))
 
-arrOfGivens = [ [94.356, 8.9752], [76.62, 128.4, 84], [20987, 3.53245, 12.345, 32.9], [], [4.35623, 8.9742]]
+arrOfGivens = [[94.356, 8.9752], [76.62, 128.4, 84], [20987, 3.53245, 12.345, 32.9], [], [4.35623, 8.9742]]
 for (let element of arrOfGivens) {
     console.log(`decimal2([ ${element.join(', ')} ])`.padEnd(pads, ' '), '-> ', decimal2(element))
+}
+
+
+/* Task 4 
+Requirement:
+ Write a function named myPow() which takes two numbers, x and n, as its arguments and returns x to the power of n without using 
+ Math.pow(). 3 to the power of 3 is 3*3*3 = 27. 
+ NOTE: Any number to the power of 0 equals 1. Any number to the power of 1 equals the number itself.
+Examples:
+ myPow(3, 3)   -> 27
+ myPow(10, 1)  -> 10
+ myPow(100, 0) -> 1
+ myPow(1, 1)   -> 1
+ myPow(4, 2)   -> 16
+ myPow(0, 0)   ​-> 1
+ myPow(5, 3)   -> 125
+*/
+console.log('\n--- Task 4 ---' + ('-'.repeat(reps)))
+const myPow = (num1, num2) => num1 ** num2
+
+arrOfGivens = [[3, 3], [10, 1], [100, 0], [1, 1], [4, 2], [0, 0], [5, 3]]
+for (let element of arrOfGivens) {
+    console.log(`myPow(${element[0]}, ${element[1]})`.padEnd(pads, ' '), '-> ', myPow(element[0], element[1]))
+}
+
+/* Task 5 
+Requirement:
+ Write a function named findLongestWord() which takes a string as input and returns the longest word in the string.
+NOTE: If the string is empty or consists of spaces only, then return empty string.
+NOTE: If the string consists of multiple words having the longest word, then return the first occurrence.
+ findLongestWord("The quick brown fox jumped over the lazy dog")       -> "jumped"
+ findLongestWord("This is a sample string for testing")                -> "testing" 
+ findLongestWord("One two ten")                                        -> "One"
+ findLongestWord("")                                                   -> ""
+ findLongestWord("      ")                                             -> ""
+*/
+console.log('\n--- Task 5 ---' + ('-'.repeat(reps)))
+const findLongestWord = str => str.trim().length === 0 ? '""' : str.trim().split(' ').reduce((acc, e) => acc.length >= e.length ? acc : e)
+
+arrOfGivens = ['The quick brown fox jumped over the lazy dog', 'This is a sample sample string for testing', 'One two ten', '', '    ']
+for (let element of arrOfGivens) {
+    console.log(`findLongestWord(${element})`.padEnd(pads, ' '), '-> ', findLongestWord(element))
 }
