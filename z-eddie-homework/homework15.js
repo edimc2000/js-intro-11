53df028fc48a8a028b0fc3da850151e5a913bc00
Eddie C. <edimc2000@gmail.com>
2025-05-28 21:49:34 -0500

    t2 added

--- z-eddie-homework/homework15.js
+++ z-eddie-homework/homework15.js
@@ -24,3 +24,31 @@ let arrOfGivens = ['first name', 'last     name', '   ZIP CODE', 'I Learn Java S
 for (let element of arrOfGivens) {
     console.log(`toCamelCase(${element})`.padEnd(pads, ' '), '-> ', toCamelCase(element))
 }
+
+
+/* Task 2
+Requirement:
+ Write a function named toSnakeCase() which takes a string as its argument and returns a new string 
+ in snake_case. Assume the string only contains letters and spaces
+NOTE: In snake case words are separated by underscores (_) and are all lowercase.
+Examples:
+ toSnakeCase("first name")              ->"first_name"
+ toSnakeCase("last    name")            ->"last_name"
+ toSnakeCase("    I love Java Script")  ->"i_love_java_script"
+ toSnakeCase("already_snake_case")      -> "already_snake_case"
+ toSnakeCase("hello")                   ​-> "hello"
+
+*/
+console.log('\n--- Task 1 ---' + ('-'.repeat(reps)))
+const toSnakeCase = str => {
+    let strArr = str.trim().split(' ').filter(e => e.length > 0)
+    let result
+    strArr.length > 1
+        ? result = strArr.map(e => e.toLowerCase()) : result = strArr
+    return result.join('_')
+}
+
+arrOfGivens = ['first name', 'last     name', ' I love Java Script', 'already_snake_case', 'hello']
+for (let element of arrOfGivens) {
+    console.log(`toSnakeCase(${element})`.padEnd(pads, ' '), '-> ', toSnakeCase(element))
+}
