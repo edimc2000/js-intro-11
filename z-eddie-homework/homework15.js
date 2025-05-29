2bf7af5acef377dd06ecdd8acd86b36c60d5f5f3
Eddie C. <edimc2000@gmail.com>
2025-05-28 21:46:03 -0500

    t1 added

--- z-eddie-homework/homework15.js
+++ z-eddie-homework/homework15.js
@@ -1,20 +1,26 @@
 const reps = 100, pads = 65
 /* Task 1 
 Requirement:
- Write a function named repeatingX() which takes a string argument and 
- returns true if the letter x is followed by another x. Otherwise, return false.
- NOTE: This method should be case-insensitive.
+ Write a function named toCamelCase() which takes a string as its argument and returns a new string
+ in camelCase. Assume the string only contains letters and spaces
 Examples:
- repeatingX("xTechxGlobalx")    -> false
- repeatingX("Hello Xx World")   -> true
- repeatingX("x x")              -> false
- repeatingX("")                 -> false
- repeatingX("xxxxx")            -> true
+ toCamelCase("first name")           ->"firstName"
+ toCamelCase("last     name")        ->"lastName"
+ toCamelCase("   ZIP CODE")          ->"zipCode"
+ toCamelCase(“I Learn Java Script”)  ->"iLearnJavaScript"
+ toCamelCase(“helloWorld”)           ->“helloWorld”
 */
 console.log('\n--- Task 1 ---' + ('-'.repeat(reps)))
-const repeatingX = str => str.toLowerCase().trim().split('').reduce((acc, e, i, arr) => (e === 'x' && arr[i + 1] === 'x') ? acc = true : acc, false)
+const toCamelCase = str => {
+    let strArr = str.trim().split(' ').filter(e => e.length > 0)
+    let result
+    strArr.length > 1
+        ? result = strArr.map((e, i) => i ? e[0].toUpperCase() + e.slice(1).toLowerCase() : e.toLowerCase())
+        : result = strArr
+    return result.join('')
+}
 
-let arrOfGivens = ['xTechxGlobalx', 'Hello Xx World', 'x x', '', 'xxxxx']
+let arrOfGivens = ['first name', 'last     name', '   ZIP CODE', 'I Learn Java Script', 'helloWorld']
 for (let element of arrOfGivens) {
-    console.log(`repeatingX(${element})`.padEnd(pads, ' '), '-> ', repeatingX(element))
+    console.log(`toCamelCase(${element})`.padEnd(pads, ' '), '-> ', toCamelCase(element))
 }
